1b82b519bd27a81e0c93637bb660ee6999abcac9
Kolotushka1 <maksim.kolotov.050605@mail.ru>
2024-03-18 22:06:10 +0700

    bit-change
    bit-change

--- cruddemo-with-data-REST/src/main/java/com/coolcode/springboot/cruddemo/dao/EmployeeRepository.java
+++ cruddemo-with-data-REST/src/main/java/com/coolcode/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.coolcode.springboot.cruddemo.dao;
 
 import com.coolcode.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // FREE CODE!
